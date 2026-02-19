5eb10b14689da8bdb3135750baaac1dee8dc7047
Felix Avalos <felavalosg@gmail.com>
2026-02-19 14:08:27 -0600

    Tag UI component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
 // Import Shared components
 import Button from './shared/Button';
+import Tag from './shared/Tag';
 
 export {
   // Export Shared components
   Button,
+  Tag,
 }
---
+++ src/components/shared/Tag.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { clsx } from 'clsx';
+
+interface Props {
+  title: string;
+  isDisable?: boolean;
+}
+
+const Tag = ({
+  title,
+  isDisable
+}: Props ) => {
+  const [isSelected, setIsSelected] = useState<boolean>(false);
+
+  const selectTag = () => {
+    if (isDisable) return;
+
+    setIsSelected(!isSelected);
+  };
+
+  return (
+    <button
+      onClick={ selectTag }
+      className={clsx(
+        "w-min text-md py-1 px-2.5 rounded-3xl",
+        isSelected
+        ? "bg-[#657153] text-white border-transparent"
+        : "bg-transparent text-[#657153] border border-[#657153]"
+    )}>
+      <p>{ title }</p>
+    </button>
+  );
+};
+
+export default Tag;
