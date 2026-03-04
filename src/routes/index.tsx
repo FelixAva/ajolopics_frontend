67ef9da9e1c141e491e84cc3715c81d597629091
Felix Avalos <felavalosg@gmail.com>
2026-03-03 18:50:25 -0600

    Index cleanning

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
-import {  SelectInput } from '../components';
-import CreatePostForm from '../components/post/CreatePostForm';
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
-
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
-]
 
 function RouteComponent() {
 
@@ -19,14 +11,6 @@ function RouteComponent() {
         <h1 className='text-3xl text-center text-black'>Welcome to the virtual gallery</h1>
         {/* <PostsMansoryGrid /> */}
         {/* <PostPreviewCard /> */}
-        <SelectInput
-          label='Select the tags'
-          name='tags'
-          isMulti
-          options={options}
-        />
-
-        <CreatePostForm />
       </div>
     );
 }
