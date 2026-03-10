ef28a42c6ef12fe8189842097ac2d8e334c73cb3
Felix Avalos <felavalosg@gmail.com>
2026-03-09 19:52:18 -0600

    vite json

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,16 +2,16 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 // Importaciones de archivos en Inglés (EN)
-import enAuth from '../public/locales/en/auth.json';
-import enComponents from '../public/locales/en/components.json';
-import enHeader from '../public/locales/en/header.json';
-import enPost from '../public/locales/en/post.json';
+import enAuth from './locales/en/auth.json';
+import enComponents from './locales/en/components.json';
+import enHeader from './locales/en/header.json';
+import enPost from './locales/en/post.json';
 
 // Importaciones de archivos en Español (ES)
-import esAuth from '../public/locales/es/auth.json';
-import esComponents from '../public/locales/es/components.json';
-import esHeader from '../public/locales/es/header.json';
-import esPost from '../public/locales/es/post.json';
+import esAuth from './locales/es/auth.json';
+import esComponents from './locales/es/components.json';
+import esHeader from './locales/es/header.json';
+import esPost from './locales/es/post.json';
 
 const resources = {
   en: {
