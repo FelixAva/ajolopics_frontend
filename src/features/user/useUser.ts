5945c2b144262bcbe269f2133cc9e9c1dc593302
Felix Avalos <felavalosg@gmail.com>
2026-03-10 21:54:28 -0600

    useUser

--- src/features/user/useUser.ts
+++ src/features/user/useUser.ts
@@ -1,18 +1,17 @@
-// import { useQuery } from '@tanstack/react-query';
-// import { UserService } from './user.service';
-// import { AxiosError } from 'axios';
-// import type { ErrorDTO } from '../../types/api.types';
-// import type { GetUserResponseDTO } from './user.api.types';
+import { useQuery } from '@tanstack/react-query';
+import { UserService } from './user.service';
+import { AxiosError } from 'axios';
+import type { ErrorDTO } from '../../types/api.types';
 
-// const useUser = () => {
-//   const getUser = useQuery<GetUserResponseDTO[], AxiosError<ErrorDTO>>({
-//     queryKey: ['users'],
-//     queryFn: () => UserService.getUser()
-//   });
+const useUser = () => {
+  const getUser = useQuery<any[], AxiosError<ErrorDTO>>({
+    queryKey: ['users'],
+    queryFn: () => UserService.getUser()
+  });
 
-//   return {
-//     getUser
-//   }
-// }
+  return {
+    getUser
+  }
+}
 
-// export default useUser;
+export default useUser;
