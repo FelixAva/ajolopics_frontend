bf902e7a7938093f4474913b879548ffe99a2f07
Felix Avalos <felavalosg@gmail.com>
2026-03-22 03:04:58 -0600

    Cursor pointer structure

--- src/features/post/components/ChangeImageComponent.tsx
+++ src/features/post/components/ChangeImageComponent.tsx
@@ -21,13 +21,13 @@ const ChangeImageComponent = ({
     <>
       <button
         onClick={prevImage}
-        className="absolute left-4 top-1/2 -translate-y-1/2 bg-white/80 hover:bg-white text-gray-800 p-2 rounded-full shadow-md transition-all"
+        className="absolute left-4 top-1/2 -translate-y-1/2 bg-white/80 hover:bg-white text-gray-800 p-2 rounded-full shadow-md transition-all cursor-pointer"
       >
         <DynamicIcon name="chevron-left" size={24} />
       </button>
       <button
         onClick={nextImage}
-        className="absolute right-4 top-1/2 -translate-y-1/2 bg-white/80 hover:bg-white text-gray-800 p-2 rounded-full shadow-md transition-all"
+        className="absolute right-4 top-1/2 -translate-y-1/2 bg-white/80 hover:bg-white text-gray-800 p-2 rounded-full shadow-md transition-all cursor-pointer"
       >
         <DynamicIcon name="chevron-right" size={24} />
       </button>
