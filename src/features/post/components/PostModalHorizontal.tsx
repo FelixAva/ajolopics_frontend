d088b5b421b192c536419dfb1a099e2e116e7129
Felix Avalos <felavalosg@gmail.com>
2026-04-05 14:17:24 -0600

    Post horizontal responsive full devices

--- src/features/post/components/PostModalHorizontal.tsx
+++ src/features/post/components/PostModalHorizontal.tsx
@@ -32,7 +32,7 @@ const PostModalHorizontal = ({
 
   return (
     <div
-      className="h-full w-full relative flex flex-col bg-white shadow-2xl overflow-hidden transition-all duration-500 ease-in-out sm:max-h-min sm:rounded-2xl sm:w-auto lg:max-h-none lg:h-auto xl:h-auto lg:flex-row"
+      className="h-full w-full relative flex flex-col bg-white shadow-2xl overflow-hidden transition-all duration-500 ease-in-out sm:max-h-min sm:rounded-2xl sm:w-auto lg:max-w-[85%] lg:max-h-none lg:h-auto xl:max-w-[75%] xl:h-auto lg:flex-row"
     >
       <Button
         onClick={onClose}
@@ -52,21 +52,21 @@ const PostModalHorizontal = ({
       ) : (
         <>
           {/* LADO IZQUIERDO: Visor de Imagen */}
-          <div className="bg-gray-100 relative flex flex-1 items-center justify-center">
+          <div className="bg-gray-100 relative flex sm:flex-1 items-center justify-center">
             {currentVariant && (
               <img
                 src={currentVariant.url}
                 alt={post.title}
-                className="w-130 sm:h-75 sm:object-cover lg:h-110 lg:w-auto xl:h-auto xl:w-186 2xl:w-250"
+                  className="w-130 sm:h-75 sm:object-cover lg:h-110 lg:w-auto lg:object-fill xl:object-cover xl:h-auto xl:w-186 2xl:w-250"
               />
             )}
             {children}
           </div>
 
           {/* LADO DERECHO: Slot mediante Composición */}
-          <div className='px-6 flex flex-col'>
+          <div className='px-6 flex flex-col lg:max-w-[40%] xl:max-w-[50%] 2xl:w-[29%]'>
             <div
-              className="h-full py-4 flex flex-col justify-start gap-4 overflow-y-auto bg-white lg:w-80 lg:py-4 xl:p-8"
+              className="h-full py-4 flex flex-col justify-start gap-4 overflow-y-auto bg-white lg:py-4"
             >
               <PostModalSide
                 post={post}
@@ -75,14 +75,14 @@ const PostModalHorizontal = ({
               />
             </div>
             <div className='py-2 sm:py-4'>
-                <Button
-                  onClick={() => alert('download')}
-                  icon='download'
-                  title={isUserAuthenticated ? t('post:detail.download', 'Download') : t('post:detail.loginToDownload')}
-                  disabled={!isUserAuthenticated}
-                  className="w-full"
-                />
-              </div>
+              <Button
+                onClick={() => alert('download')}
+                icon='download'
+                title={isUserAuthenticated ? t('post:detail.download', 'Download') : t('post:detail.loginToDownload')}
+                disabled={!isUserAuthenticated}
+                className="w-full"
+              />
+            </div>
 
           </div>
         </>
