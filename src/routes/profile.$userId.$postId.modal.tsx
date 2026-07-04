4c0f2b74e0cab8f016c8399f692d759c0d4d5d96
Felix Avalos <felavalosg@gmail.com>
2026-07-03 23:53:03 -0600

    New post modal from profile

---
+++ src/routes/profile.$userId.$postId.modal.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/profile/$userId/$postId/modal')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return <div>Hello "/profile/$userId/posts/$postId/modal"!</div>
+}
