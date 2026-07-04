faa55db582a6f5783aea5660118527f6e018cc27
Felix Avalos <felavalosg@gmail.com>
2026-07-03 21:08:59 -0600

    User profile add design, load user data

--- src/routes/profile.$userId.tsx
+++ src/routes/profile.$userId.tsx
@@ -1,8 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link, Outlet } from '@tanstack/react-router'
 import { createSeoHead } from '@/utils/seo';
 import { getSeoTranslation } from '@/utils/seoTranslations';
+import Button from '@/components/ui/Button';
+import { userProfileQueryOptions } from '@/features/user/api/user.query-options';
 
 export const Route = createFileRoute('/profile/$userId')({
+  loader: async ({context: { queryClient }, params: { userId }}) => {
+    const user = await queryClient.ensureQueryData(userProfileQueryOptions(userId))
+    return { user };
+  },
   head: ({ params: { userId } }) => createSeoHead({
     title: getSeoTranslation('profile.title'),
     description: getSeoTranslation('profile.description'),
@@ -13,5 +19,39 @@ export const Route = createFileRoute('/profile/$userId')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/profile"!</div>
+  const { userId } = Route.useParams();
+  const { user } = Route.useLoaderData();
+
+  return (
+    <main>
+      <section className='h-80 flex items-center justify-center'>
+        <img src='https://placehold.co/200' className='w-62.5 h-62.5 rounded-full' />
+
+        <div className='pl-5 self-center'>
+          <div className='flex gap-3'>
+            <h2 className='text-xl'>{user.name}</h2>
+
+            <>
+              <Button icon='pencil' variant='none' size='none' />
+              <Button icon='share' variant='none' size='none' />
+            </>
+          </div>
+          <h3>@{user.username}</h3>
+        </div>
+      </section>
+
+      <nav>
+        <Link
+          to="/profile/$userId"
+          params={{ userId }}
+        >Posts</Link>
+      </nav>
+
+      <section>
+        {/* Masonry */}
+      </section>
+
+      <Outlet />
+    </main>
+  );
 }
