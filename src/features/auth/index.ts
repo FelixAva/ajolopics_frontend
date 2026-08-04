e2318222d6b5f403c1e901308cff84e0f2347906
Felix Avalos <felavalosg@gmail.com>
2026-08-04 17:29:43 -0600

    Add barrel file to auth feature

---
+++ src/features/auth/index.ts
@@ -0,0 +1,21 @@
+// Componentes
+export * from './components/LogoutModal';
+
+// Hooks
+export { default as useAuth } from './hooks/useAuth';
+
+// Schemas
+export * from './schemas/auth.schemas';
+
+// Services
+export * from './services/auth.service';
+
+// Store
+export * from './store/auth.store';
+
+// Types
+export type * from './types/auth.api.types';
+export type * from './types/auth.store.types';
+
+// Utils
+export * from './utils/auth.constants';
